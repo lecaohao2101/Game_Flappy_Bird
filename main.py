d855c25013874e14e03078dff337ffc33d1aa348
Your Name <you@example.com>
2023-02-19 22:53:33 +0700

    draw lower tube opposite above tube

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import pygame
 from random import randint
 
 pygame.init()
-screen = pygame.display.set_mode((400,600))
+WIDTH, HEIGHT = 400, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Flappy Bird')
 running = True
 
@@ -13,6 +14,7 @@ clock = pygame.time.Clock()
 
 TUBE_WIDTH = 50
 TUBE_VELOCITY = 3
+TUBE_GAP = 150
 TUBE_DISTANCE = 550
 
 tube1_x = 0
@@ -26,21 +28,30 @@ tube3_height = randint(100,400)
 while running:
     clock.tick(60)
     screen.fill(GREEN)
+#Draw tube inverse
+    pygame.draw.rect(screen, BLUE, (tube1_x, tube1_height + TUBE_GAP, TUBE_WIDTH, HEIGHT - tube1_height - TUBE_GAP))
+    pygame.draw.rect(screen, BLUE, (tube2_x, tube2_height + TUBE_GAP, TUBE_WIDTH, HEIGHT - tube2_height - TUBE_GAP))
+    pygame.draw.rect(screen, BLUE, (tube3_x, tube3_height + TUBE_GAP, TUBE_WIDTH, HEIGHT - tube3_height - TUBE_GAP))
 
     pygame.draw.rect(screen, BLUE, (tube1_x, 0, TUBE_WIDTH, tube1_height))
     pygame.draw.rect(screen, BLUE, (tube2_x, 0, TUBE_WIDTH, tube2_height))
     pygame.draw.rect(screen, BLUE, (tube3_x, 0, TUBE_WIDTH, tube3_height))
 
+#move tube to the left
     tube1_x = tube1_x - 3
     tube2_x = tube2_x - 3
     tube3_x = tube3_x - 3
 
+#Generate new tubes
     if tube1_x < -TUBE_WIDTH:
         tube1_x = 550
+        tube1_height = randint(100,400)
     if tube2_x < -TUBE_WIDTH:
         tube2_x = 550
+        tube2_height = randint(100, 400)
     if tube3_x < -TUBE_WIDTH:
         tube3_x = 550
+        tube3_height = randint(100, 400)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
